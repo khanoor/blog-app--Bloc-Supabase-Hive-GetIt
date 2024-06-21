fa7461cf8310e581f175bbd39b52fc84bc831e14
Noor <khannooralam31@gmail.com>
2024-06-22 01:07:20 +0530

    -

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,46 @@
+import 'package:blog_app/core/error/expection.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String email,
+    required String name,
+    required String password,
+  });
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRomoteDataSourceImpl extends AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  AuthRomoteDataSourceImpl({required this.supabaseClient});
+  @override
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String email,
+    required String name,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth
+          .signUp(password: password, email: email, data: {
+        'name': name,
+      });
+      if (response.user == null) {
+        throw const ServerException("User is null");
+      }
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
